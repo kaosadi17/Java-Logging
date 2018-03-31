46b7bb7c13315d7cc37aef5a82b7d9978fbaa58f
amalio <kaosadi17@gmail.com>
2018-03-31 11:48:31 +0530

    added properties file and finalize proect

---
+++ Logging/src/Log4j.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+public class Log4j {
+	
+	private static final Logger log = Logger.getLogger(Log4j.class);
+	
+	public static void main(String args[]){
+		
+		log.info("Test message");
+	}
+
+}
